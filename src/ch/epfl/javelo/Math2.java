b0f3bbb91da41117ef69302dd06ba756b65af0a4
alexmrt25 <alexandre.mourot@epfl.ch>
2022-03-25 14:00:18 +0100

    Partie 5

--- src/ch/epfl/javelo/Math2.java
+++ src/ch/epfl/javelo/Math2.java
@@ -124,10 +124,12 @@ public class Math2 {
      * @return - double : The length of the projection of AP vector on the AB vector.
      */
     public static double projectionLength(double aX, double aY, double bX, double bY, double pX, double pY) {
-        double l = dotProduct(pX - aX, pY - aY, bX - aX, bY - aY);
-        double k = squaredNorm(bX - aX, bY - aY);
-        double factor = l / k;
-        return norm(factor * (bX - aX), factor * (bY - aY));
+        double vX = pX-aX;
+        double vY = pY-aY;
+        double wX = bX-aX;
+        double wY = bY-aY;
+
+        return dotProduct(vX, vY, wX, wY) / norm(wX, wY);
     }
 }
 
